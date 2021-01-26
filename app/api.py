4d9fc2021aacaee21cb740cb38b905e935f8d952
Alin <alin_florin.mihaila@stud.acs.upb.ro>
2021-01-26 20:45:05 +0200

    🚀 model inference works

--- app/api.py
+++ app/api.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from newsplease import NewsPlease
 from pydantic import BaseModel
 
+from app.model import load_model, get_embedding
+
 
 class ClassificationReport(BaseModel):
     title: str
@@ -26,6 +28,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+model = load_model()
+
 
 def get_article_info(url):
     article = NewsPlease.from_url(url)
@@ -35,8 +39,17 @@ def get_article_info(url):
 @app.get("/predict", response_model=ClassificationReport)
 async def predict_news(url: str):
     title, body = get_article_info(url)
+    embedding = get_embedding(title + " " + body)
+    result = model.predict(embedding)[0]
+    result_class = "True"
+    confidence = result[1]
+
+    if result[0] > result[1]:
+        result_class = "False"
+        confidence = result[0]
+
     return ClassificationReport(
         title=title,
-        confidence=0.89,
-        class_="c"
+        confidence=float("{:.2f}".format(confidence * 100)),
+        class_=result_class
     )
---
+++ app/model.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from nltk.tokenize import sent_tokenize
+from tensorflow.keras import backend as K
+from transformers import BertTokenizer, TFBertModel
+import numpy as np
+
+NB_OF_SENTS = 30
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert = TFBertModel.from_pretrained("bert-base-uncased")
+
+
+def get_model(lstm_cell_size=150):
+    in_id = tf.keras.layers.Input((NB_OF_SENTS, 768), name="input_shape")
+    lstm_later, forward_h, forward_c = tf.keras.layers.LSTM(lstm_cell_size, return_sequences=True, return_state=True)(
+        in_id)
+    linear = tf.keras.layers.Dense(lstm_cell_size)(forward_h)
+    attention = tf.keras.layers.dot([lstm_later, linear], axes=(-1))
+    attention = tf.keras.layers.Activation('softmax', name='attention_vec')(attention)
+    attention = tf.keras.layers.RepeatVector(lstm_cell_size)(attention)
+    attention = tf.keras.layers.Permute([2, 1])(attention)
+    sent_representation = tf.keras.layers.multiply([lstm_later, attention])
+    sent_representation = tf.keras.layers.Lambda(lambda xin: K.sum(xin, axis=1))(sent_representation)
+    sent_representation_final = tf.keras.layers.Concatenate()([sent_representation, forward_h])
+    drop = tf.keras.layers.Dropout(0.2)(sent_representation_final)
+    predictions = tf.keras.layers.Dense(2, activation='softmax')(drop)
+    model = tf.keras.Model(inputs=in_id, outputs=predictions)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['acc'])
+    return model
+
+
+def get_embedding(text: str):
+    text = text.lower()
+    sents = sent_tokenize(text)
+    snt_good = []
+    for idx, snt in enumerate(sents):
+        if idx == NB_OF_SENTS:
+            break
+        snt_good.append(snt)
+    while len(snt_good) != NB_OF_SENTS:
+        snt_good.append('')
+
+    assert (len(snt_good) == NB_OF_SENTS)
+    encoded_inputs = tokenizer(snt_good, padding=True, truncation=True, return_tensors="tf", max_length=60)
+    output = bert(encoded_inputs)
+    y = tf.keras.layers.GlobalAveragePooling1D()(output[0])
+    y = np.reshape(y, (-1, 30, 768))
+    return y
+
+
+def load_model():
+    model = get_model()
+    model.load_weights(r'model_weights/')
+    return model
